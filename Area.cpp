9abf29a62ae6870b54f3d2377b94cf3fee085aa9
Peter <pforbes@nd.edu>
2014-03-21 11:55:28 -0400

    Create Area.cpp

---
+++ Area.cpp
@@ -0,0 +1,71 @@
+#include "Area.h"
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+Area::Area(string path) {
+	fileName = path;
+
+	ifstream areaFile(path.c_str());
+
+	vector<char> temp;
+	string currentLine;
+	int i;
+
+	while(!areaFile.eof()) {
+		
+		temp.clear();
+		getline(areaFile,currentLine); 
+		
+		for(i = 0; i < currentLine.length(); i++) {
+			if(currentLine[i] != ' ') {
+				temp.push_back(currentLine[i]);
+			}
+		}
+		
+		tileIndex.push_back(temp);
+	
+	}
+
+	width = i;
+	height = tileIndex[0].size();
+
+}
+
+Area::Area(int areaW, int areaH) {
+	srand(time(NULL));
+
+	height = areaH;
+	width = areaW;
+
+	vector<char> temp;
+
+	for(int y = 0; y < height; y++) {
+		
+		for(int x = 0; x < width; x++) {
+			if(rand() % 2 == 0) {
+				temp.push_back('G');
+			} else {
+				temp.push_back('I');
+			}
+		}
+
+		tileIndex.push_back(temp);
+		temp.clear();
+
+	}
+}
+
+int Area::getWidth() {
+	return width;
+}
+
+int Area::getHeight() {
+	return height;
+}
+
+char Area::getTile(int x, int y) {
+	return tileIndex[y][x];
+}
