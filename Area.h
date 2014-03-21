419b42ea369a12bf9d74da5c850e930cbc6ec44f
Peter <pforbes@nd.edu>
2014-03-21 11:52:04 -0400

    Create Area.h

---
+++ Area.h
@@ -0,0 +1,21 @@
+#include <vector>
+#include <string>
+using namespace std;
+
+#ifndef AREA_H
+#define AREA_H
+
+class Area {
+public:
+	Area(string);
+	Area(int, int);
+	int getWidth();
+	int getHeight();
+	char getTile(int,int);
+private:
+	int width, height;
+	vector< vector<char>> tileIndex;
+	string fileName;
+};
+
+#endif
