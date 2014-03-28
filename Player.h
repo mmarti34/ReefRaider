41bb36d026f0a50d4876b3a1cbf13e61d9abd484
Peter <pforbes@nd.edu>
2014-03-28 19:04:32 -0400

    Update Player.h - add projectiles, land/sea transition

--- Player.h
+++ Player.h
@@ -1,34 +1,35 @@
 #include "SpriteSheet.h"
+#include "Mobile.h"
+#include "Projectile.h"
+#include "Display.h"
+#include "Ship.h"
 #include "Keyboard.h"
 #include "Sprite.h"
+#include <string>
+using namespace std;
 
 #ifndef PLAYER_H
 #define PLAYER_H
 
-class Player {
+class Player : public Mobile {
 public:
-	Player();
+	Player(string);
 	~Player();
+	void update(Display*); 
+	void renderAttrib(SDL_Surface*, Display* display);
 	void render(int, int, SDL_Surface*);
-	void update();
-	void setLocation(int, int);
-	int getX();
-	int getY(); 
+	bool isSailing();
 private:
-	int x, y;
-	int direction;
+	void processInput(Display* display);
+	void renderLand(int, int, SDL_Surface*);
+	void renderSea(int, int, SDL_Surface*);
+	void toBoat(Display*);
+	void fireProjectile();
+	Ship* playerVessel;
+	Projectile* left_projectile;
+	Projectile* right_projectile;
 	Keyboard* keyboard;
-	SpriteSheet playerSprites;
-	Sprite left;
-	Sprite right;
-	Sprite up;
-	Sprite down;
-	Sprite move_left;
-	Sprite move_right;
-	Sprite move_down1;
-	Sprite move_down2;
-	Sprite move_up1;
-	Sprite move_up2;
+	bool shipState;
 };
 
 #endif
