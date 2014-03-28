b2e2480e03ea79df731555917ade1109683d5736
Peter <pforbes@nd.edu>
2014-03-28 19:08:39 -0400

    Update Sprite.h - overloaded ==, adds size options

--- Sprite.h
+++ Sprite.h
@@ -6,15 +6,23 @@
 
 class Sprite {
 public:
-	Sprite(SDL_Surface*, int, int);
+	Sprite(SDL_Surface*, int, int, int, bool, bool);
+	bool operator==(const Sprite &) const;
 	void render(int, int, SDL_Surface*);
-	int getSize();
+	static int getSize();
+	static int getExpSize();
+	bool isSolid();
+	bool isSliding();
+	bool isDirectional();
 private:
 	void loadSprite();
-	static int size;
 	int x, y;					// x and y are the coordinates on the sheet, not pixel coordinates
 	SDL_Surface* sheet;
 	SDL_Rect sprite;
+	bool solid;
+	bool sliding;
+	bool directional;
+	int size;
 };
 
 #endif
