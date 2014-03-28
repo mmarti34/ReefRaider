9cbffc0ed8032b749fa56b63194c684c79d87571
Peter <pforbes@nd.edu>
2014-03-28 18:59:51 -0400

    Changed rendering method, added functions

--- Display.h
+++ Display.h
@@ -4,6 +4,7 @@
 #include "SDL.h"
 #include "SDL_image.h"
 #include <string>
+using namespace std;
 
 #ifndef DISPLAY_H
 #define DISPLAY_H
@@ -15,14 +16,19 @@ public:
 	int getHeight();
 	void setOffsets(int, int);
 	void render(SDL_Surface*);
+	char* getCurrentTile();
+	Sprite* getSprite(int x, int y);
+	void getOffsets();
+	void ctrIncrement();
+	Sprite* getWaterHandle(int);
 private:
+	void renderTile(int, int, Sprite*, SDL_Surface*);
 	int width, height;
 	int xOffset, yOffset;
+	int ctr;
 	SpriteSheet terrainSheet;
-	Sprite ice;
-	Sprite grass;
-	Sprite flower;
-	Sprite bush;
+	Sprite ice, grass, bush, flower, sand;
+	Sprite water1, water2, fence_front, fence_left, fence_right;
 	Area mainArea;
 };
 
