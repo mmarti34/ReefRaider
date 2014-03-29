acae4c01c1c46165d4760bfe50cdb25a286165e5
Peter <pforbes@nd.edu>
2014-03-28 21:09:26 -0400

    Update Sprite.cpp - overloaded ==

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,12 +1,23 @@
 #include "Sprite.h"
 
-int Sprite::size = 16;
+//int Sprite::size = 16;
+//int Sprite::expSize = 4;
 
-Sprite::Sprite(SDL_Surface* spriteSheet, int xcoord, int ycoord) {
+Sprite::Sprite(SDL_Surface* spriteSheet, int xcoord, int ycoord, int sprSize, bool solidTile, bool slidingTile) {
 	x = xcoord;
 	y = ycoord;
+	size = sprSize;
+	solid = solidTile;
+	sliding = slidingTile;
 	sheet = spriteSheet;
 	loadSprite();
+}
+
+bool Sprite::operator==(const Sprite &sprite) const {
+	if((x == sprite.x) && (y == sprite.y) && (sheet == sprite.sheet)) {
+		return true;
+	}
+	return false;
 }
 
 void Sprite::loadSprite() {
@@ -25,6 +36,18 @@ void Sprite::render(int xScreen, int yScreen, SDL_Surface* destination) {
 	SDL_BlitSurface(sheet, &sprite, destination, &screenOffset);
 }
 
-int Sprite::getSize() {
-	return size;
+//int Sprite::getSize() {
+//	return size;
+//}
+
+//int Sprite::getExpSize() {
+//	return expSize;
+//}
+
+bool Sprite::isSolid() {
+	return solid;
+}
+
+bool Sprite::isSliding() {
+	return sliding;
 }
