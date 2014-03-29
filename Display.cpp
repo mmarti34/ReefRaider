8159e49ecd299f5d39f7b29db1128e5875f7c43c
Peter <pforbes@nd.edu>
2014-03-28 20:55:59 -0400

    Update Display.cpp - fixed rendering

--- Display.cpp
+++ Display.cpp
@@ -1,20 +1,40 @@
 #include "Display.h"
-#include <math.h>
+#include <iostream>
+#include <cmath>
+//#include <Windows.h>
+#include <sstream>
 #include <string>
+using namespace std;
 
 Display::Display(int WIDTH, int HEIGHT) 
-	:terrainSheet( "Terrain.png", 256),
+	:terrainSheet( "Terrain.png" ),
 
-	ice(terrainSheet.getSurface(), 0, 0),
-	grass(terrainSheet.getSurface(), 0, 1),
-	flower(terrainSheet.getSurface(), 1, 1),
-	bush(terrainSheet.getSurface(), 2, 1), 
+	ice(terrainSheet.getSurface(), 0, 0, 16, false, true),
+	grass(terrainSheet.getSurface(), 0, 1, 16, false, false),
+	flower(terrainSheet.getSurface(), 1, 1, 16, false, false),
+	bush(terrainSheet.getSurface(), 2, 1, 16, true, false), 
+	sand(terrainSheet.getSurface(), 3, 1, 16, false, false),
+	water1(terrainSheet.getSurface(), 5, 1, 16, true, false),
+	water2(terrainSheet.getSurface(), 1,0, 16, true, false),
+	fence_front(terrainSheet.getSurface(), 0, 2, 16, true, false),
+	fence_left(terrainSheet.getSurface(), 0, 3, 16, true, false),
+	fence_right(terrainSheet.getSurface(), 0, 4, 16, true, false),
 
-	mainArea(70,50) {
+	mainArea("MainMap.txt") {
 
 		width = WIDTH;
 		height = HEIGHT;
+		xOffset = 0;
+		yOffset = 0;
+		ctr = 0;
+}
 
+int Display::getWidth() {
+	return width;
+}
+
+int Display::getHeight() {
+	return height;
 }
 
 void Display::setOffsets(int x, int y) {
@@ -22,33 +42,113 @@ void Display::setOffsets(int x, int y) {
 	yOffset = y;
 }
 
+void Display::renderTile(int x, int y, Sprite* sprite, SDL_Surface* screen) {
+	int xRender = x - xOffset % 16;
+	int yRender = y - yOffset % 16;
+
+	if(xOffset < 0) {
+		xRender = x + (abs(xOffset) % 16);
+	}
+	if(yOffset < 0) {
+		yRender = y + (abs(yOffset) % 16);
+	}
+
+	(*sprite).render(xRender, yRender, screen);
+}
+
 void Display::render(SDL_Surface* screen) {
-	int xScreen;
-	int yScreen;
+	ctr++;
+	int xTileMin = xOffset >> 4;
+	int yTileMin = yOffset >> 4;
+	int xTileMax = (xOffset + width + 16) >> 4;
+	int yTileMax = (yOffset + height + 16) >> 4;
 
-	for(int y = (yOffset - 16) >> 4; y < ((yOffset + height + 16) >> 4); y++) {
-		for(int x = (xOffset - 16) >> 4; x < ((xOffset + width + 16) >> 4); x++) {
-
-			xScreen = x - (xOffset >> 4);
-			yScreen = y - (yOffset >> 4);
-			
-			if(x >= mainArea.getWidth() || y >= mainArea.getHeight() || y < 0 || x < 0) {
-				grass.render((xScreen * 16) - (xOffset % 16), (yScreen * 16) - (yOffset % 16), screen);
-			}
-			else {
-
-				switch(mainArea.getTile(x,y)) {
-				case 'G':
-					grass.render((xScreen * 16) - (xOffset % 16), (yScreen * 16) - (yOffset % 16), screen);
-					break;
-				case 'I':
-					ice.render((xScreen * 16) - (xOffset % 16), (yScreen * 16) - (yOffset % 16), screen);
-					break;
-				case 'A':
-					flower.render((xScreen * 16) - (xOffset % 16), (yScreen * 16) - (yOffset % 16), screen);
-					break;
-				}
-			}
+	for(int y = yTileMin; y < yTileMax; y++) {
+		for(int x = xTileMin; x < xTileMax; x++) {
+			renderTile(((x - xTileMin) << 4), ((y - yTileMin) << 4), getSprite(x, y), screen);
 		}
+	}
+}
+
+Sprite* Display::getSprite(int x, int y) {
+	if(x < 0 || y < 0 || x >= mainArea.getWidth() - 1 || y >= mainArea.getHeight() - 1) {
+		return &grass;
+	}
+
+	switch(mainArea.getTile(x, y)) {
+	case 'G': 
+		return &grass;
+		break;
+	case 'I':
+		return &ice;
+		break;
+	case 'A':
+		return &flower;
+		break;
+	case 'B':
+		return &bush;
+		break;
+	case 'S':
+		return &sand;
+		break;
+	case 'W':
+		if(ctr % 280 < 140) {
+			return &water1;
+		} else {
+			return &water2;
+		}
+		break;
+	}
+}
+
+char* Display::getCurrentTile() {
+	int xTile = (xOffset + 8 + width / 2) >> 4;
+	int yTile = (yOffset + 8 + height / 2) >> 4;
+
+	char tile = '&';
+
+	if((xTile >= 0) && (yTile >= 0) && (xTile < mainArea.getWidth()) && (yTile < mainArea.getHeight())) {
+		tile = mainArea.getTile(xTile, yTile);
+	}
+
+	switch(tile) {
+	case 'G':
+		return "grass";
+		break;
+	case 'I':
+		return "ice";
+		break;
+	case 'A':
+		return "flower";
+		break;
+	case 'B':
+		return "bush";
+		break;
+	case 'S':
+		return "sand";
+		break;
+	case 'W':
+		return "water";
+		break;
+	default:
+		return "tile not found";
+	}
+}
+
+/* void Display::getOffsets() {
+	stringstream ss;
+	ss << "X: " << xOffset << " Y: " << yOffset;
+	OutputDebugString(ss.str().c_str());
+} */
+
+void Display::ctrIncrement() {
+	ctr++;
+}
+
+Sprite* Display::getWaterHandle(int which) {
+	if(which == 1) {
+		return &water1;
+	} else {
+		return &water2;
 	}
 }
